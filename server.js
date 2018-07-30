4456960014835458b022793ae61f757254a744ee
atkinsta <atkinstaa@gmail.com>
2018-07-30 10:49:33 -0400

    Basic server setup, basic views

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-import express from "express";
-
-
+const express = require("express");
 const app = express();
+const PORT = 4040;
+
+// setting up Pug
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+require("./routes/backendroutes")(app);
+require("./routes/htmlroutes")(app);
+
+app.listen(PORT, () => {
+    console.log("Listening on port");
+});
 
